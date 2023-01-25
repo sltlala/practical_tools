468553d0c75e7d53cda6349a6b7ad87df66ecc9e
sltlala <sltlala@163.com>
2023-01-26 02:26:45 +0800

    更新

--- 视频爬虫.py
+++ 视频爬虫.py
@@ -12,9 +12,9 @@ os.chdir("../")
 # 无字幕 腾讯 会员视频 MP4
 os.system("yt-dlp --merge-output-format 'mp4' --external-downloader 'aria2c' --cookies-from-browser edge --downloader-args aria2c:'-x 16 -k 1M' ")
 # 无字幕 油管 频道全视频 MP4 频道+标题
-os.system("yt-dlp -f 'bv+ba' --merge-output-format 'mp4' -o '%(channel)s/%(title)s.%(ext)s' --downloader-args aria2c:'-x 16 -k 1M' ")
+os.system("yt-dlp -f 'bv+ba' --merge-output-format 'mp4' -o '%(channel)s/%(title)s.%(ext)s' --external-downloader aria2c --downloader-args aria2c:'-x 16 -k 1M' ")
 
-os.system("yt-dlp -f 'bv+ba' --merge-output-format 'mp4' -o '%(channel)s/%(title)s.%(ext)s' --downloader-args aria2c:'-x 16 -k 1M' ")
+os.system("yt-dlp -f 'bv+ba' --merge-output-format 'mp4' -o '%(channel)s/%(title)s.%(ext)s' --external-downloader aria2c --downloader-args aria2c:'-x 16 -k 1M' ")
 
 # 中文,英语,英语(美),日语字幕 油管 MKV
 os.system("yt-dlp -f 'bv+ba' --embed-subs --sub-langs 'zh.*,en.*,jp,fr,de,ko' --merge-output-format 'mkv' --downloader-args aria2c:'-x 16 -k 1M' ")
@@ -22,3 +22,5 @@ os.system("yt-dlp -f 'bv+ba' --embed-subs --sub-langs 'zh.*,en.*,jp,fr,de,ko' --
 os.system("yt-dlp -f 'bv+ba' --embed-subs --sub-langs 'zh.*,en.*,jp,fr,de,ko' --embed-thumbnail --embed-metadata --merge-output-format 'mkv' --external-downloader aria2c  --downloader-args aria2c:'-x 16 -k 1M ' https://www.youtube.com/watch?v=8lUgdf-zkQMM")
 
 os.system("yt-dlp -f 'bv+ba' --embed-thumbnail --embed-metadata --merge-output-format 'mp4' --external-downloader aria2c  --downloader-args aria2c:'-x 16 -k 1M' https://www.bilibili.com/video/BV11K411o7Gd/")
+
+os.system("yt-dlp -f 'bv+ba' --embed-subs --sub-langs 'zh.*,en.*,ja' --embed-thumbnail --embed-metadata --merge-output-format 'mkv' --external-downloader aria2c  --downloader-args aria2c:'-x 16 -k 1M' https://www.youtube.com/watch?v=e1xCOsgWG0M")
